90bc5cd9894a3300030d4859434ea3f6da632ad3
kcatms00 <seryan5311@gmail.com>
2024-08-06 13:39:51 +0900

    admin

--- Backend/src/main/java/com/tms/backend/service/AdminService.java
+++ Backend/src/main/java/com/tms/backend/service/AdminService.java
@@ -2,7 +2,7 @@
 package com.tms.backend.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,11 +16,18 @@ import lombok.extern.log4j.Log4j;
 @Service
 @Log4j
 public class AdminService {
+	
+	@Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private AdminMapper adminmapper;
 
     public void join(User user) {
+    	log.info(user);
+    	String encodedPassword = passwordEncoder.encode(user.getPassword());
+    	log.info(encodedPassword);
+    	user.setPassword(encodedPassword);
 		adminmapper.insert(user);
 	}
 }
