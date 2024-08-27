6e64c175e884e6d9f49fc45ebd07e503d05b7a12
kcatms00 <seryan5311@gmail.com>
2024-08-27 13:54:04 +0900

    feat: 분류코드 등록 및 수정 기능 추가

--- Backend/src/main/webapp/WEB-INF/views/categoryCode.jsp
+++ Backend/src/main/webapp/WEB-INF/views/categoryCode.jsp
@@ -1,44 +1,228 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html lang="ko">
-<head>
-    <meta charset="UTF-8">
-    <title>카테고리 코드 목록</title>
-    <style>
-        table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-        th, td {
-            padding: 8px;
-            text-align: left;
-            border: 1px solid #ddd;
-        }
-        th {
-            background-color: #f4f4f4;
-        }
-        a {
-            margin: 0 5px;
-        }
-        .center {
-            text-align: center;
-        }
-    </style>
-</head>
-<body>
-<h1>카테고리 코드 목록</h1>
+  <head>
+    <%@ include file="./common.jsp" %>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" type="text/css" href="../../resources/css/categoryCode.css" />
+
+    <title>TMS 분류코드 관리</title>
+    <script type="module" src="../../resources/js/categoryCode.js"></script>
+  </head>
+
+  <body>
+    <header class="header">
+      <!-- 공통 헤더 정보 동적으로 삽입-->
+    </header>
+
+    <main class="content">
+      <h1 class="page-title">분류코드 관리</h1>
+
+      <!-- Register Modal -->
+      <div id="categoryCodeRegisterModal" class="modal">
+        <!-- Modal content -->
+        <div class="modal-content">
+          <h2>분류코드 등록</h2>
+          <form id="categoryCodeRegisterForm" class="modal-form">
+            <div class="modal-form-group">
+              <label for="stageTypeForRegister">구분</label>
+              <div class="flex-box">
+                <label>
+                  <input type="radio" id="mainCategoryForRegister" name="stageType" value="대" checked />
+                  대분류
+                </label>
+                <label>
+                  <input type="radio" id="subCategoryForRegister" name="stageType" value="중" />
+                  중분류
+                </label>
+              </div>
+            </div>
+            <div class="modal-form-group">
+              <label for="parentCodeForRegister">대분류</label>
+              <select id="parentCodeForRegister" name="parentCode" disabled>
+                <option value="" selected disabled>대분류를 선택해주세요</option>
+                <!-- 대분류 목록 동적 삽입 -->
+              </select>
+            </div>
+
+            <div class="modal-form-group">
+              <label for="codeForRegister">코드</label>
+              <div class="code-display">
+                <span id="partialCodeForRegister"></span>
+                <input
+                  type="text"
+                  id="codeForRegister"
+                  name="code"
+                  placeholder="숫자 2자리 입력(추후 변경불가)"
+                  required
+                  pattern="\d{2}"
+                  maxlength="2"
+                />
+              </div>
+            </div>
+
+            <div class="modal-form-group">
+              <label for="codeNameForRegister">코드명</label>
+              <input
+                type="text"
+                id="codeNameForRegister"
+                name="codeName"
+                required
+                placeholder="코드명을 입력해주세요"
+              />
+            </div>
+
+            <div class="flex-box justify-end">
+              <button type="button" id="closeCategoryCodeRegisterModalButton" class="cancel-button">취소</button>
+              <button type="submit" class="save-button">저장</button>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <!-- Edit Modal -->
+      <div id="categoryCodeEditModal" class="modal">
+        <!-- Modal content -->
+        <div class="modal-content">
+          <h2>분류코드 수정</h2>
+          <form id="categoryCodeEditForm" class="modal-form">
+            <div class="modal-form-group">
+              <label for="stageTypeForEdit">구분</label>
+              <span id="stageTypeForEdit"></span>
+            </div>
+
+            <div class="modal-form-group">
+              <label for="codeForEdit">코드</label>
+              <span id="codeForEdit"></span>
+            </div>
+
+            <div class="modal-form-group">
+              <label for="codeNameForEdit">코드명</label>
+              <input type="text" id="codeNameForEdit" name="codeName" required placeholder="코드명을 입력해주세요" />
+            </div>
+
+            <div class="flex-box justify-end">
+              <button type="button" id="closeCategoryCodeEditModalButton" class="cancel-button">취소</button>
+              <button type="submit" class="save-button">저장</button>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <!-- File Download Modal -->
+      <div id="categoryCodeFileDownloadModal" class="modal">
+        <!-- Modal content -->
+        <div class="modal-content">
+          <h2>엑셀 다운로드</h2>
+          <div class="flex-box">
+            <form id="downloadAllCategoryCodeForm" action="downloadAllcc" method="get">
+              <button type="submit" id="downloadAllCategoryCodeButton" class="excel-button">
+                <img src="../../resources/images/download_icon.png" />전체자료 다운로드
+              </button>
+            </form>
+
+            <form
+              id="downloadFilteredCategoryCodeForm"
+              action="downloadFilteredcc"
+              method="get"
+              id="downloadFilteredForm"
+            >
+              <!-- 숨겨진 input 필드 -->
+              <input type="hidden" id="parentCodeForDownload" name="parentCode" />
+              <input type="hidden" id="codeForDownload" name="code" />
+              <input type="hidden" id="codeNameForDownload" name="codeName" />
+
+              <button type="submit" id="downloadFilteredCategoryCodeButton" class="excel-button">
+                <img src="../../resources/images/download_icon.png" />
+                조회결과 다운로드
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      <!-- 분류코드 조회 필터링 폼 -->
+      <form id="categoryCodeFilterForm">
+        <div class="filter-container">
+          <div class="form-group">
+            <label for="parentCodeForFilter">대분류</label>
+            <select id="parentCodeForFilter" name="parentCode">
+              <option value="" selected>전체</option>
+            </select>
+          </div>
+
+          <div class="form-group">
+            <label for="codeForFilter">중분류</label>
+            <select id="codeForFilter" name="code">
+              <option value="" selected>전체</option>
+            </select>
+          </div>
+
+          <div class="form-group">
+            <label for="codeNameForFilter">코드명</label>
+            <input type="text" id="codeNameForFilter" name="codeName" placeholder="코드명을 입력하세요" />
+          </div>
+        </div>
+        <div class="flex-box align-end">
+          <button type="submit" id="searchCategoryCodeButton">조회</button>
+          <button type="reset" id="resetButton" class="reset-button">초기화</button>
+        </div>
+      </form>
+
+      <!-- 버튼 그룹 -->
+      <div class="button-group">
+        <div class="flex-box">
+          <button id="openCategoryCodeRegisterModalButton">등록</button>
+          <button id="deleteCategoryCodeButton" class="delete-button">삭제</button>
+        </div>
+        <div class="flex-box">
+          <!-- 파일 선택 input -->
+          <input type="file" id="uploadCategoryCodeFileInput" name="file" accept=".xlsx, .xls" />
+
+          <button id="uploadCategoryCodeFileButton" class="excel-button">
+            <img src="../../resources/images/upload_icon.png" />엑셀 업로드
+          </button>
+          <button id="openCategoryCodeFileDownloadModalButton" class="excel-button">
+            <img src="../../resources/images/download_icon.png" />엑셀 다운로드
+          </button>
+        </div>
+      </div>
+
+      <!-- 분류코드 목록 테이블 -->
+      <div class="table-container">
+        <table id="categoryCodeTable">
+          <thead>
+            <tr>
+              <th><input type="checkbox" id="selectAllCategoryCodeCheckbox" /></th>
+              <th class="th-stage-type">단계구분</th>
+              <th class="th-code">코드</th>
+              <th class="th-code-name">코드명</th>
+            </tr>
+          </thead>
+          <tbody id="categoryCodeTableBody">
+            <!-- 분류코드 정보 동적으로 삽입-->
+          </tbody>
+        </table>
+      </div>
+
+      <!-- 페이지네이션 -->
+      <div id="categoryCodePagination" class="pagination">
+        <!-- 페이지네이션 버튼 동적으로 삽입 -->
+      </div>
+    </main>
+  </body>
+</html>
 
 <!-- 검색 폼 -->
-<form method="get" action="/tms/categoryCode">
+<!-- <form method="get" action="/tms/categoryCode">
     <input type="text" name="stageType" placeholder="StageType" value="${param.stageType}" />
     <input type="text" name="code" placeholder="Code" value="${param.code}" />
     <input type="text" name="codeName" placeholder="CodeName" value="${param.codeName}" />
     <button type="submit">검색</button>
-</form>
+</form> -->
 
 <!-- 엑셀 다운로드 -->
-<h2>엑셀 기능</h2>
+<!-- <h2>엑셀 기능</h2>
 <form method="get" action="/tms/downloadAllcat">
     <button type="submit">전체 엑셀 다운로드</button>
 </form>
@@ -47,17 +231,17 @@
     <input type="hidden" name="code" value="${param.code}" />
     <input type="hidden" name="codeName" value="${param.codeName}" />
     <button type="submit">검색된 결과 엑셀 다운로드</button>
-</form>
+</form> -->
 
 <!-- 엑셀 업로드 -->
-<h2>엑셀 업로드</h2>
+<!-- <h2>엑셀 업로드</h2>
 <form method="post" action="/tms/api/catupload" enctype="multipart/form-data">
     <input type="file" name="file" accept=".xlsx" />
     <button type="submit">업로드</button>
-</form>
+</form> -->
 
 <!-- 카테고리 코드 테이블 -->
-<table>
+<!-- <table>
     <thead>
         <tr>
             <th>StageType</th>
@@ -93,26 +277,10 @@
             </tr>
         </c:if>
     </tbody>
-</table>
-
-<!-- 페이징 링크 -->
-<div>
-    <c:forEach var="i" begin="1" end="${totalPages}">
-        <c:choose>
-            <c:when test="${i == currentPage}">
-                <strong>${i}</strong>
-            </c:when>
-            <c:otherwise>
-                <a href="?page=${i}&size=${param.size}&stageType=${param.stageType}&code=${param.code}&codeName=${param.codeName}">
-                    ${i}
-                </a>
-            </c:otherwise>
-        </c:choose>
-    </c:forEach>
-</div>
+</table> -->
 
 <!-- 등록 폼 -->
-<h2>카테고리 코드 등록</h2>
+<!-- <h2>카테고리 코드 등록</h2>
 <form action="/tms/catwrite" method="post">
     <label for="stageType">StageType:</label>
     <select id="stageType" name="stageType">
@@ -120,8 +288,7 @@
         <option value="중">중</option>
     </select><br/>
 
-    <!-- 상위 대분류 코드 입력 필드 -->
-    <label for="parentCode">Parent Code:</label>
+ <label for="parentCode">Parent Code:</label>
     <input type="text" id="parentCode" name="parentCode" /><br/>
 
     <label for="code">Code:</label>
@@ -130,15 +297,5 @@
     <label for="codeName">CodeName:</label>
     <input type="text" id="codeName" name="codeName" required /><br/>
     
-    <button type="submit">등록</button>
-</form>
-
-<!-- 상태 메시지 출력 -->
-<c:if test="${not empty message}">
-    <div class="status ${status}">
-        ${message}
-    </div>
-</c:if>
-
-</body>
-</html>
+    <button type="submit">등록</button> 
+ </form> -->
---
+++ Backend/src/main/webapp/resources/js/categoryCode.js
@@ -0,0 +1,505 @@
+import {
+  tmsFetch,
+  openModal,
+  closeModal,
+  closeModalOnClickOutside,
+  renderTMSHeader,
+  showSpinner,
+  hideSpinner,
+  setupPagination,
+} from "./common.js";
+
+let currentPage = 1;
+
+// DOM 요소들
+const uploadCategoryCodeFileInput = document.getElementById("uploadCategoryCodeFileInput");
+const uploadCategoryCodeFileButton = document.getElementById("uploadCategoryCodeFileButton");
+
+const categoryCodeRegisterForm = document.getElementById("categoryCodeRegisterForm");
+const categoryCodeEditForm = document.getElementById("categoryCodeEditForm");
+const categoryCodeFilterForm = document.getElementById("categoryCodeFilterForm");
+
+const parentCodeSelect = document.getElementById("parentCodeForFilter");
+const parentCodeForRegister = document.getElementById("parentCodeForRegister");
+const mainCategoryForRegister = document.getElementById("mainCategoryForRegister");
+const subCategoryForRegister = document.getElementById("subCategoryForRegister");
+
+const categoryCodeTableBody = document.getElementById("categoryCodeTableBody");
+const selectAllCategoryCodeCheckbox = document.getElementById("selectAllCategoryCodeCheckbox");
+
+const openCategoryCodeRegisterModalButton = document.getElementById("openCategoryCodeRegisterModalButton");
+const openCategoryCodeFileDownloadModalButton = document.getElementById("openCategoryCodeFileDownloadModalButton");
+const closeCategoryCodeRegisterModalButton = document.getElementById("closeCategoryCodeRegisterModalButton");
+const closeCategoryCodeEditModalButton = document.getElementById("closeCategoryCodeEditModalButton");
+const deleteCategoryCodeButton = document.getElementById("deleteCategoryCodeButton");
+
+// 문서 로드 시 초기화
+document.addEventListener("DOMContentLoaded", init);
+
+// 모달 아이디
+const MODAL_ID = {
+  COMMON_CODE_REGISTER: "categoryCodeRegisterModal",
+  COMMON_CODE_EDIT: "categoryCodeEditModal",
+  COMMON_CODE_FILE_DOWNLOAD: "categoryCodeFileDownloadModal",
+};
+
+// 초기화 함수
+function init() {
+  renderTMSHeader();
+  setupEventListeners();
+  loadInitialCategoryCodes();
+}
+
+// 이벤트 핸들러 설정
+function setupEventListeners() {
+  if (parentCodeSelect) {
+    parentCodeSelect.addEventListener("change", () => {
+      const selectedParentCode = parentCodeSelect.value;
+
+      initializeChildCodes(selectedParentCode);
+    });
+  }
+
+  // 분류코드 파일 업로드 버튼 클릭 이벤트 핸들러
+  if (uploadCategoryCodeFileButton) {
+    uploadCategoryCodeFileButton.addEventListener("click", () => {
+      if (uploadCategoryCodeFileInput) {
+        uploadCategoryCodeFileInput.click(); // 파일 선택 창 열기
+      } else {
+        console.error("File input element not found.");
+      }
+    });
+  }
+
+  // 분류코드 파일 업로드 인풋 change 이벤트 핸들러
+  if (uploadCategoryCodeFileInput) {
+    uploadCategoryCodeFileInput.addEventListener("change", uploadCategoryCodeFile);
+  }
+
+  // 분류코드 테이블 클릭 이벤트 핸들러
+  if (categoryCodeTableBody) {
+    // 분류코드 테이블에서 클릭된 행의 데이터 로드 (이벤트 위임)
+    categoryCodeTableBody.addEventListener("click", (event) => {
+      const clickedElement = event.target;
+      if (clickedElement.type === "checkbox") {
+        return;
+      }
+
+      const row = event.target.closest("tr");
+
+      if (row) {
+        loadCategoryCodeDataFromRow(row);
+      }
+    });
+  }
+
+  // 분류코드 등록 폼 이벤트 핸들러
+  if (categoryCodeRegisterForm) {
+    categoryCodeRegisterForm.addEventListener("submit", registerCategoryCode);
+
+    mainCategoryForRegister.addEventListener("change", () => {
+      // 대분류 선택 시 대분류 목록 잠금
+      if (mainCategoryForRegister.checked) {
+        parentCodeForRegister.value = "";
+        parentCodeForRegister.disabled = true;
+
+        document.getElementById("partialCodeForRegister").textContent = "";
+      }
+    });
+
+    subCategoryForRegister.addEventListener("change", () => {
+      // 중분류 선택 시 대분류 목록 fetch
+      if (subCategoryForRegister.checked) {
+        parentCodeForRegister.disabled = false;
+        initializeParentCodes("parentCodeForRegister");
+      }
+    });
+
+    parentCodeForRegister.addEventListener("change", () => {
+      // 대분류 선택 시 코드 앞2자리 render
+      document.getElementById("partialCodeForRegister").textContent = parentCodeForRegister.value;
+    });
+  }
+
+  // 분류코드 수정 폼 제출 이벤트 핸들러
+  if (categoryCodeEditForm) {
+    categoryCodeEditForm.addEventListener("submit", editCategoryCode);
+  }
+
+  // 분류코드 필터 폼 제출 및 리셋 이벤트 핸들러
+  if (categoryCodeFilterForm) {
+    categoryCodeFilterForm.addEventListener("submit", submitCategoryCodeFilter);
+    categoryCodeFilterForm.addEventListener("reset", resetCategoryCodeFilter);
+  }
+
+  // 분류코드 삭제 버튼 클릭 이벤트 핸들러
+  if (deleteCategoryCodeButton) {
+    deleteCategoryCodeButton.addEventListener("click", deleteCategoryCode);
+  }
+
+  // 전체 선택 체크박스 클릭 이벤트 핸들러
+  if (selectAllCategoryCodeCheckbox) {
+    selectAllCategoryCodeCheckbox.addEventListener("click", toggleAllCheckboxes);
+  }
+
+  // 모달 열기 및 닫기 버튼 이벤트 핸들러
+  if (
+    openCategoryCodeRegisterModalButton &&
+    closeCategoryCodeRegisterModalButton &&
+    openCategoryCodeFileDownloadModalButton &&
+    closeCategoryCodeEditModalButton
+  ) {
+    openCategoryCodeRegisterModalButton.addEventListener("click", () => {
+      openModal(MODAL_ID.COMMON_CODE_REGISTER);
+    });
+    closeCategoryCodeRegisterModalButton.addEventListener("click", () => closeModal(MODAL_ID.COMMON_CODE_REGISTER));
+
+    openCategoryCodeFileDownloadModalButton.addEventListener("click", () => {
+      copyFilterValuesToDownloadForm(); // 분류코드 필터링 값 복사
+      openModal(MODAL_ID.COMMON_CODE_FILE_DOWNLOAD); // 모달 열기
+    });
+
+    closeCategoryCodeEditModalButton.addEventListener("click", () => closeModal(MODAL_ID.COMMON_CODE_EDIT));
+  }
+
+  // 모달 외부 클릭 시 닫기 버튼 이벤트 핸들러
+  setupModalEventListeners();
+}
+
+function setupModalEventListeners() {
+  const modals = Object.values(MODAL_ID); // 모든 모달 ID 배열
+
+  modals.forEach((modalId) => {
+    // 모달 외부 클릭 시 닫기 설정
+    window.addEventListener("click", (event) => closeModalOnClickOutside(event, modalId));
+  });
+}
+
+// 대분류 목록 조회
+async function getCTParentCodes() {
+  try {
+    const { parentCodes = [] } = await tmsFetch("/catparentCode");
+
+    return { parentCodes };
+  } catch (error) {
+    console.error("Error fetching CC Parent Codes", error.message);
+  }
+}
+
+// 코드 목록 조회
+async function getCCCodes(parentCode = "") {
+  try {
+    if (parentCode === "") return [];
+
+    const query = new URLSearchParams({ parentCode }).toString();
+    const { CTCodes = [] } = await tmsFetch(`/catcode?${query}`);
+
+    return { codes: CTCodes };
+  } catch (error) {
+    console.error("Error fetching CC Codes", error.message);
+  }
+}
+
+// 대분류 목록을 가져와 select 요소에 옵션을 설정하는 함수
+async function initializeParentCodes(selectElementId) {
+  const { parentCodes } = await getCTParentCodes();
+  const parentCodeSelect = document.getElementById(selectElementId);
+
+  // 기존 옵션을 모두 삭제하고, 기본값이 되는 옵션은 따로 저장
+  const defaultOption = parentCodeSelect.querySelector("option[selected]");
+  parentCodeSelect.innerHTML = ""; // 기존 옵션들 삭제
+
+  // 기본값 옵션을 다시 추가
+  if (defaultOption) {
+    parentCodeSelect.appendChild(defaultOption);
+  }
+
+  // 새 옵션 생성 및 추가
+  parentCodes.forEach((parentCode) => {
+    const option = document.createElement("option");
+
+    option.value = parentCode.code;
+    option.textContent = parentCode.codeName;
+    parentCodeSelect.appendChild(option);
+  });
+}
+
+// 대분류 선택 시, 코드 목록을 가져와 select 요소에 옵션을 설정하는 함수
+async function initializeChildCodes(selectedParentCode) {
+  const codeSelect = document.getElementById("codeForFilter");
+
+  // 기존 옵션 초기화하고 "전체" 옵션은 남겨두기
+  codeSelect.innerHTML = '<option value="">전체</option>';
+
+  if (selectedParentCode === "") return; // 대분류 '전체'를 선택한 경우
+
+  const { codes } = await getCCCodes(selectedParentCode);
+
+  // 새 옵션 생성 및 추가
+  codes.forEach((code) => {
+    const option = document.createElement("option");
+
+    option.value = code.code;
+    option.textContent = code.codeName;
+    codeSelect.appendChild(option);
+  });
+}
+
+/**
+ * 분류코드 데이터를 API에서 비동기적으로 가져옵니다.
+ * 이 함수는 선택적 필터와 페이지 번호를 사용하여 분류코드 목록을 가져오고 표시합니다.
+ *
+ * @param {Object} params - 분류코드 데이터를 가져오기 위한 필터 및 페이지 정보.
+ * @param {number} [params.page=1] - 가져올 페이지 번호 (기본값: 1).
+ * @param {string} [params.categoryCodeName=""] - 분류코드 이름으로 필터링할 문자열 (기본값: 빈 문자열).
+ * @param {string} [params.authorityName=""] - 권한 이름으로 필터링할 문자열 (기본값: 빈 문자열).
+ *
+ * @throws {Error} API 요청 실패 시 오류를 발생시킵니다.
+ *
+ * @returns {Promise<void>} 이 함수는 결과를 반환하지 않습니다. 성공적으로 데이터를 가져온 후,
+ *                          `displayCategoryCodes` 함수를 호출하여 분류코드 목록을 페이지에 표시합니다.
+ */
+async function getCategoryCodes({ page = 1, parentCode = "", code = "", codeName = "" } = {}) {
+  try {
+    const query = new URLSearchParams({ page, parentCode, code, codeName }).toString();
+    const { categoryCodes, totalPages } = await tmsFetch(`/categoryCode?${query}`);
+
+    return { categoryCodes, totalPages };
+  } catch (error) {
+    console.error("Error fetching categoryCodes:", error);
+  }
+}
+
+// 분류코드 목록 테이블 렌더링
+async function renderCategoryCodes({ page = 1, parentCode = "", code = "", codeName = "" } = {}) {
+  const { categoryCodes, totalPages } = await getCategoryCodes({ page, parentCode, code, codeName });
+
+  if (categoryCodeTableBody) {
+    categoryCodeTableBody.innerHTML = "";
+
+    categoryCodes.forEach((categoryCode) => {
+      const row = document.createElement("tr");
+
+      row.innerHTML = `
+        <td><input type="checkbox" name="categoryCode" value="${categoryCode.code}"></td>
+        <td class="stage-type">${categoryCode.stageType}</td>
+        <td class="code">${categoryCode.code}</td>
+        <td class="code-name">${categoryCode.codeName}</td>
+      `;
+      categoryCodeTableBody.appendChild(row);
+    });
+
+    setupPagination({ paginationElementId: "categoryCodePagination", totalPages, currentPage, changePage });
+  }
+}
+
+// 페이지 변경
+function changePage(page) {
+  currentPage = page;
+
+  const parentCode = document.getElementById("parentCodeForFilter").value;
+  const code = document.getElementById("codeForFilter").value;
+  const codeName = document.getElementById("codeNameForFilter").value.trim();
+
+  renderCategoryCodes({ page: currentPage, parentCode, code, codeName });
+}
+
+// 분류코드 필터링
+function submitCategoryCodeFilter(event) {
+  event.preventDefault(); // 폼 제출 기본 동작 방지
+
+  // 페이지를 1로 초기화하고 renderCategoryCodes 호출
+  currentPage = 1;
+
+  const parentCode = document.getElementById("parentCodeForFilter").value;
+  const code = document.getElementById("codeForFilter").value;
+  const codeName = document.getElementById("codeNameForFilter").value.trim();
+
+  renderCategoryCodes({ page: currentPage, parentCode, code, codeName });
+}
+
+// 분류코드 필터 리셋
+function resetCategoryCodeFilter() {
+  this.reset(); // 폼 초기화
+}
+
+// HTML 요소에서 분류코드 정보를 가져와서 분류코드 수정 폼에 미리 채우기
+async function loadCategoryCodeDataFromRow(row) {
+  const stageType = row.querySelector(".stage-type").textContent.trim();
+  const code = row.querySelector(".code").textContent.trim();
+  const codeName = row.querySelector(".code-name").textContent.trim();
+
+  // 폼에 분류코드 정보 입력
+  document.getElementById("stageTypeForEdit").textContent = stageType;
+  document.getElementById("codeForEdit").textContent = code;
+  document.getElementById("codeNameForEdit").value = codeName;
+
+  // 모달 열기
+  openModal(MODAL_ID.COMMON_CODE_EDIT);
+}
+
+// 분류코드 등록
+async function registerCategoryCode(event) {
+  event.preventDefault(); // 폼 제출 기본 동작 방지
+
+  const confirmed = confirm("분류코드를 등록하시겠습니까?");
+
+  if (!confirmed) return;
+
+  const formData = new FormData(event.target);
+
+  // FormData 객체를 JSON 객체로 변환
+  const formDataObj = Object.fromEntries(formData.entries());
+
+  try {
+    showSpinner();
+
+    const { categoryCode, status } = await tmsFetch("/catwrite", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formDataObj),
+    });
+
+    const success = status === "success";
+
+    if (success) {
+      alert(`분류코드(코드명: ${categoryCode.codeName}) 등록이 완료되었습니다.`);
+      event.target.reset(); // 폼 초기화
+      closeModal(MODAL_ID.COMMON_CODE_REGISTER); // 모달 닫기
+      window.location.reload();
+    }
+  } catch (error) {
+    alert(error.message + "\n다시 시도해주세요.");
+  } finally {
+    hideSpinner();
+  }
+}
+
+// 분류코드 수정
+async function editCategoryCode(event) {
+  event.preventDefault(); // 폼 제출 기본 동작 방지
+
+  const formData = new FormData(event.target);
+
+  // FormData 객체를 JSON 객체로 변환
+  const formDataObj = Object.fromEntries(formData.entries());
+  const code = document.getElementById("codeForEdit").textContent;
+  const stageType = document.getElementById("stageTypeForEdit").textContent;
+
+  formDataObj.code = code;
+  formDataObj.stageType = stageType;
+
+  try {
+    showSpinner();
+    const { categoryCode, status } = await tmsFetch("/catmodify", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formDataObj),
+    });
+
+    const success = status === "success";
+
+    if (success) {
+      alert(`분류코드(코드명: ${categoryCode.codeName}) 수정이 완료되었습니다.`);
+      event.target.reset(); // 폼 초기화
+      closeModal(MODAL_ID.COMMON_CODE_EDIT); // 모달 닫기
+      location.reload(); // 페이지 새로고침
+    }
+  } catch (error) {
+    alert(error.message + "\n다시 시도해주세요.");
+  } finally {
+    hideSpinner();
+  }
+}
+
+// 체크박스 전체 선택/해제
+function toggleAllCheckboxes() {
+  const checkboxes = document.querySelectorAll('input[name="categoryCode"]');
+  checkboxes.forEach((checkbox) => (checkbox.checked = selectAllCategoryCodeCheckbox.checked));
+}
+
+// 분류코드 삭제
+async function deleteCategoryCode() {
+  try {
+    const confirmed = confirm("분류코드를 삭제하시겠습니까?");
+
+    if (!confirmed) return;
+
+    const selectedCategoryCodeIDs = Array.from(document.querySelectorAll('input[name="categoryCode"]:checked')).map(
+      (checkbox) => checkbox.value
+    );
+
+    if (selectedCategoryCodeIDs.length === 0) {
+      alert("삭제할 분류코드를 선택해주세요.");
+      return;
+    }
+
+    showSpinner();
+
+    const { status } = await tmsFetch("/catdelete", {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(selectedCategoryCodeIDs),
+    });
+
+    const success = status === "success";
+
+    if (success) {
+      alert(`분류코드 삭제가 완료되었습니다.`);
+      location.reload(); // 페이지 새로고침
+    }
+  } catch (error) {
+    alert(error.message + "\n다시 시도해주세요.");
+  } finally {
+    hideSpinner();
+  }
+}
+
+// 분류코드 파일 업로드
+async function uploadCategoryCodeFile() {
+  if (uploadCategoryCodeFileInput.files.length <= 0) return;
+
+  const formData = new FormData();
+
+  // key 이름의 경우 서버와 협의
+  formData.append("file", uploadCategoryCodeFileInput.files[0]);
+
+  try {
+    showSpinner();
+
+    const response = await tmsFetch("/catupload", {
+      method: "POST",
+      body: formData,
+    });
+
+    const success = response ? response.status === "success" : false;
+
+    if (success) {
+      alert("파일 업로드가 완료되었습니다.");
+      location.reload(); // 페이지 새로고침
+    }
+  } catch (error) {
+    alert(error.message + "\n다시 시도해주세요.");
+  } finally {
+    hideSpinner();
+  }
+}
+
+// 분류코드 필터링 폼의 값을 다운로드 폼으로 복사하는 함수
+function copyFilterValuesToDownloadForm() {
+  // 분류코드 조회 필터링 폼의 값을 가져옴
+  const parentCode = document.getElementById("parentCodeForFilter").value;
+  const code = document.getElementById("codeForFilter").value;
+  const codeName = document.getElementById("codeNameForFilter").value;
+
+  // 숨겨진 다운로드 폼의 input 필드에 값을 설정
+  document.getElementById("parentCodeForDownload").value = parentCode;
+  document.getElementById("codeForDownload").value = code;
+  document.getElementById("codeNameForDownload").value = codeName;
+}
+
+// 초기 분류코드 목록 로드
+function loadInitialCategoryCodes() {
+  initializeParentCodes("parentCodeForFilter"); // 필터링 폼 select 요소에 대분류 render
+  renderCategoryCodes(); // 분류코드 목록 render
+}
